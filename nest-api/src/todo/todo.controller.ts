0942a2c5d7caa86ab13885f547875c62a2507214
haruki suziuki <harukiti166@gmail.com>
2023-07-06 19:01:23 +0900

    todoコントローラー、サービス追加

--- nest-api/src/todo/todo.controller.ts
+++ nest-api/src/todo/todo.controller.ts
@@ -1,4 +1,70 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  UseGuards,
+  Req,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { TodoService } from './todo.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
+import { Task } from '@prisma/client';
 
+@UseGuards(AuthGuard('jwt'))
 @Controller('todo')
-export class TodoController {}
+export class TodoController {
+  constructor(private readonly todoService: TodoService) {}
+
+  //ユーザーのタスクをすべて取得API
+  @Get()
+  getTasks(@Req() req: Request): Promise<Task[]> {
+    ////jwt.strategy.tsのvalidateの返り値が
+    //NestJSによってリクエストに含まれるためそれをgetTasksに渡す
+    return this.todoService.getTasks(req.user.id);
+  }
+
+  //特定のタスクを取得API
+  @Get(':id')
+  getTaskById(
+    @Req() req: Request,
+    //パラメータの:idを数値型にパースしてそれをtaskIdにぶち込む
+    @Param('id', ParseIntPipe) taskId: number,
+  ): Promise<Task> {
+    return this.todoService.getTaskById(req.user.id, taskId);
+  }
+
+  //新規タスクを作成API
+  @Post()
+  createTask(@Req() req: Request, @Body() dto: CreateTaskDto): Promise<Task> {
+    return this.todoService.createTask(req.user.id, dto);
+  }
+
+  //タスク更新API
+  @Patch(':id')
+  updateTaskById(
+    @Req() req: Request,
+    @Param('id', ParseIntPipe) taskId: number,
+    @Body() dto: UpdateTaskDto,
+  ): Promise<Task> {
+    return this.todoService.updateTaskById(req.user.id, taskId, dto);
+  }
+
+  //タスク削除API
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Delete(':id')
+  deleteTaskById(
+    @Req() req: Request,
+    @Param('id', ParseIntPipe) taskId: number,
+  ): Promise<void> {
+    return this.todoService.deleteTaskById(req.user.id, taskId);
+  }
+}
--- nest-api/src/todo/todo.service.ts
+++ nest-api/src/todo/todo.service.ts
@@ -1,4 +1,99 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ForbiddenException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
+import { Task } from '@prisma/client';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  //あるユーザーのすべてのタスクを取得する
+  getTasks(userId: number): Promise<Task[]> {
+    //引数で受けとったuserIdのtaskのみを取得する
+    return this.prisma.task.findMany({
+      where: {
+        userId,
+      },
+      orderBy: {
+        createAt: 'desc',
+      },
+    });
+  }
+
+  //特定のタスクを取得
+  getTaskById(userId: number, taskId: number): Promise<Task> {
+    return this.prisma.task.findFirst({
+      where: {
+        userId,
+        id: taskId,
+      },
+    });
+  }
+
+  //新規のタスクを作成する
+  async createTask(userId: number, dto: CreateTaskDto): Promise<Task> {
+    const newTask = await this.prisma.task.create({
+      data: {
+        userId,
+        ...dto, //dtoがオブジェクトのため展開している
+      },
+    });
+    return newTask;
+  }
+
+  //タスクを更新する
+  async updateTaskById(
+    userId: number,
+    taskId: number,
+    dto: UpdateTaskDto,
+  ): Promise<Task> {
+    //更新しようとしているtaskIdのタスクが存在するのか確認
+    const task = await this.prisma.task.findUnique({
+      where: {
+        id: taskId,
+      },
+    });
+
+    ////タスクが存在しない場合または見つかってもそのタスクのuserIdとログインしているuserIdが
+    //一致しない時にエラーを投げる
+    if (!task || task.userId !== userId) {
+      throw new ForbiddenException('No permision to update');
+    }
+
+    ////タスクが存在していてかつそのタスクのuserIdとログインしているuserIdが
+    //一致している時のアップデートの処理
+    return this.prisma.task.update({
+      where: {
+        id: taskId,
+      },
+      data: {
+        ...dto,
+      },
+    });
+  }
+
+  //タスクを削除する
+  async deleteTaskById(userId: number, taskId: number): Promise<void> {
+    //削除したいタスクのtaskIdが存在するのか確認
+    const task = await this.prisma.task.findUnique({
+      where: {
+        id: taskId,
+      },
+    });
+
+    ////タスクが見つからない、または見つかったタスクのuserIdとログインしているuserIdが
+    //一致しない場合エラーを投げる
+    if (!task || task.userId !== userId) {
+      throw new ForbiddenException('No permision to delete');
+    }
+
+    ////タスクが見つかり、かつそのタスクのuserIdとログインしているユーザーの
+    //userIdが一致する時の削除の処理
+    await this.prisma.task.delete({
+      where: {
+        id: taskId,
+      },
+    });
+  }
+}
