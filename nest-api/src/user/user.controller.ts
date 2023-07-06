9c80f5ee34c344dc9212254f121b6638e7943208
haruki suziuki <harukiti166@gmail.com>
2023-07-06 14:18:43 +0900

    むずいこと！。、、やりました

--- nest-api/src/user/user.controller.ts
+++ nest-api/src/user/user.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Req, Patch, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { UserService } from './user.service';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from '@prisma/client';
 
+//JWTトークンベースの認証を実装し、セキュリティを強化することができる。
+//AuthGuardはリクエストが有効なJWTトークンを持っているかどうかを検証し認証が成功した場合にリクエストを処理
+@UseGuards(AuthGuard('jwt'))
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  getLoginUser(@Req() req: Request): Omit<User, 'hashedPassword'> {
+    return req.user;
+  }
+  //Omit<User, 'hashedPassword'>:User型からhashedPasswordプロパティを取り除いた新しい型を生成
+
+  //ニックネームを更新するAPI
+  @Patch()
+  updateUser(
+    @Req() req: Request,
+    @Body() dto: UpdateUserDto,
+  ): Promise<Omit<User, 'hashedPassword'>> {
+    return this.userService.updateUser(req.user.id, dto);
+  }
+}
--- nest-api/src/user/user.service.ts
+++ nest-api/src/user/user.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from '@prisma/client';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  //ユーザー情報を更新するメソッド
+  async updateUser(
+    userId: number,
+    dto: UpdateUserDto,
+  ): Promise<Omit<User, 'hashedPassword'>> {
+    //更新されたユーザー情報がuserという変数に代入
+    const user = await this.prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        ...dto,
+      },
+    });
+    //userオブジェクトからhashedPasswordプロパティを削除しています。
+    //パスワードのハッシュ化された値をクライアントに返さないため
+    delete user.hashedPassword;
+    return user;
+  }
+}
