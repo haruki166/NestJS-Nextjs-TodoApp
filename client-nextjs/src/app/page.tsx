cf815ea10222eb943571f81d443c45b8c7b9ba55
haruki suziuki <harukiti166@gmail.com>
2023-07-07 19:39:44 +0900

    ログインページ作成

--- client-nextjs/src/app/page.tsx
+++ client-nextjs/src/app/page.tsx
@@ -1,95 +1,125 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import type { NextPage } from "next";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import * as Yup from "yup";
+import { IconDatabase } from "@tabler/icons";
+import { ShieldCheckIcon } from "@heroicons/react/solid";
+import { ExclamationCircleIcon } from "@heroicons/react/outline";
+import {
+  Anchor,
+  TextInput,
+  Button,
+  Group,
+  PasswordInput,
+  Alert,
+} from "@mantine/core";
+import { useForm, yupResolver } from "@mantine/form";
+import Layout from "@/components/Layout";
+import { AuthForm } from "../../types/index";
+
+const schema = Yup.object().shape({
+  email: Yup.string().email("Invaid email").required("No email provided"),
+  password: Yup.string()
+    .required("No password provided")
+    .min(5, "Password should be min 5 chars"),
+});
 
 export default function Home() {
+  const router = useRouter();
+  const [isRegister, setIsRegister] = useState(false);
+  const [error, setError] = useState("");
+  const form = useForm<AuthForm>({
+    validate: yupResolver(schema),
+    initialValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const handleSunbit = async () => {
+    //isRegisterがtrueなら登録のAPIを叩いてその後ログインAPIを叩く
+    //falseならそのままログインAPIを叩く。
+    try {
+      //新規登録の場合の処理。
+      if (isRegister) {
+        await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/signup`, {
+          //ここがdtoとしてNestJSのRestAPIに渡される。
+          email: form.values.email,
+          password: form.values.password,
+        });
+      }
+      await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
+        email: form.values.email,
+        password: form.values.password,
+      });
+      form.reset(); //formの値を初期値にする。
+      router.push("/dashboard"); //dashboardページに遷移する。
+    } catch (error: any) {
+      setError(error.response.data.message);
+    }
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <Layout title="Auth">
+      <ShieldCheckIcon className="h-16 w-16 text-blue-500" />
+      {/* errorに値が入っている時だけMantainUIのAlertコンポーネントを表示させる */}
+      {error && (
+        <Alert
+          my="md"
+          variant="filled"
+          icon={<ExclamationCircleIcon />}
+          title="Authorization Error"
+          color="red"
+          radius="md"
+        >
+          {error}
+        </Alert>
+      )}
+      <form onSubmit={form.onSubmit(handleSunbit)}>
+        <TextInput
+          mt="md"
+          id="email"
+          label="Email*"
+          placeholder="example@gmail.com"
+          //したの記述でformのemailに入力した値が自動的に入る！
+          {...form.getInputProps("email")}
         />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        <PasswordInput
+          mt="md"
+          id="password"
+          label="Password*"
+          placeholder="password"
+          description="Must be min 5 char"
+          //したの記述でformのpasswordに入力した値が自動的に入る！
+          {...form.getInputProps("password")}
+        />
+        <Group mt="xl" position="apart">
+          <Anchor
+            component="button"
+            type="button"
+            size="xs"
+            className="text-gray-300"
+            onClick={() => {
+              setIsRegister(!isRegister);
+              console.log(isRegister);
+              setError("");
+            }}
+          >
+            {isRegister
+              ? "Have an account? Login"
+              : "Don't have an account? Register"}
+          </Anchor>
+          <Button
+            leftIcon={<IconDatabase size={14} />}
+            color="cyan"
+            type="submit"
+          >
+            {isRegister ? "Register" : "Login"}
+          </Button>
+        </Group>
+      </form>
+    </Layout>
+  );
 }
